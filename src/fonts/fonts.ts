080ddf02a92080a406f66ae44c38d4abe319f7df
Илья Семёнов <ilya.semyonov.03@mail.ru>
2024-09-25 11:13:05 +0300

    feat: project basic settings

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from 'next/font/local'
+import { Rubik } from 'next/font/google'
+
+export const sharpGroteskBook20 = localFont({
+    src: './SharpGrotesk-Book20.otf',
+    style: 'normal',
+    variable: '--font-sharp-groteskB20',
+})
+
+export const sharpGroteskMedium20 = localFont({
+    src: './SharpGrotesk-Medium20.otf',
+    style: 'normal',
+    variable: '--font-sharp-groteskM20',
+})
+export const sharpGroteskMedium25 = localFont({
+    src: './SharpGrotesk-Medium25.otf',
+    style: 'normal',
+    variable: '--font-sharp-groteskM25',
+})
+
+export const rubik = Rubik({ subsets: ['latin'] })
